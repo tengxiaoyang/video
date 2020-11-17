839e2dfa3fa0ff3126fb25c05993cdfc5a23b7da
txy <656877449@qq.com>
2020-11-17 21:31:44 +0800

    新增或修改

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -51,7 +51,7 @@
           v-if="video_content.indexOf(video_content_item)!==0"
         >
           <span class="title">{{video_content_item.title}}</span>
-          <div v-if="video_content">
+          <div v-if="video_content_item.list.length%2===0">
             <!-- <div class="video_list_container"> -->
             <div class="video_list_container" 
               v-if="!video_content_item.change_block_list"
@@ -104,6 +104,142 @@
                 </span>
               </div>
             </div>
+          </div>
+
+          
+          <div v-if="video_content_item.list.length%2!==0">
+
+            <div class="video_list_top_container"
+              v-if="!video_content_item.change_block_list"
+            >
+              <div class="video_list_top" 
+                v-for="(video_list_item, video_list_index) of video_content_item.list.slice(0, 1)"
+                :key="video_list_index" 
+                @click="route_to('video_details')"
+              >
+                <div class="img_container">
+                  <img lazyload :src="video_list_item.img" alt="">
+                  <div class="top_left_corner">
+                    <div class="triangle"></div>
+                    <span class="free" v-if="video_list_item.vip===0">免费</span>
+                    <span class="vip" v-if="video_list_item.vip===10">VIP</span>
+                  </div>
+                  <div class="bottom_right_corner">{{video_list_item.score}}</div>
+                </div>
+                <span class="name">
+                  {{video_list_item.name}}
+                </span>
+                <span class="summary">
+                  {{video_list_item.summary}}
+                </span>
+              </div>
+            </div>
+
+            <!-- <div class="video_list_container"> -->
+            <div class="video_list_container" 
+              v-if="!video_content_item.change_block_list"
+            >
+              <div class="video_list" 
+                v-for="(video_list_item, video_list_index) of video_content_item.list.slice(1, video_content_item.list.length)"
+                :key="video_list_index" 
+                @click="route_to('video_details')"
+              >
+                <div class="img_container">
+                  <img lazyload :src="video_list_item.img" alt="">
+                  <div class="top_left_corner">
+                    <div class="triangle"></div>
+                    <span class="free" v-if="video_list_item.vip===0">免费</span>
+                    <span class="vip" v-if="video_list_item.vip===10">VIP</span>
+                  </div>
+                  <div class="bottom_right_corner">{{video_list_item.score}}</div>
+                </div>
+                <span class="name">
+                  {{video_list_item.name}}
+                </span>
+                <span class="summary">
+                  {{video_list_item.summary}}
+                </span>
+              </div>
+            </div>
+
+
+            <div class="video_list_top_container"
+              v-if="video_content_item.change_block_list"
+            >
+              <div class="video_list_top" 
+                v-for="(video_list_item, video_list_index) of video_content_item.list.slice(video_content_item.list.length-1, video_content_item.list.length)"
+                :key="video_list_index" 
+                @click="route_to('video_details')"
+              >
+                <div class="img_container">
+                  <img lazyload :src="video_list_item.img" alt="">
+                  <div class="top_left_corner">
+                    <div class="triangle"></div>
+                    <span class="free" v-if="video_list_item.vip===0">免费</span>
+                    <span class="vip" v-if="video_list_item.vip===10">VIP</span>
+                  </div>
+                  <div class="bottom_right_corner">{{video_list_item.score}}</div>
+                </div>
+                <span class="name">
+                  {{video_list_item.name}}
+                </span>
+                <span class="summary">
+                  {{video_list_item.summary}}
+                </span>
+              </div>
+            </div>
+
+            <!-- <div class="video_list_container"> -->
+            <div class="video_list_container" 
+              v-if="video_content_item.change_block_list"
+            >
+              <div class="video_list" 
+                v-for="(video_list_item, video_list_index) of video_content_item.list.slice(0, video_content_item.list.length-1).reverse()"
+                :key="video_list_index" 
+                @click="route_to('video_details')"
+              >
+                <div class="img_container">
+                  <img lazyload :src="video_list_item.img" alt="">
+                  <div class="top_left_corner">
+                    <div class="triangle"></div>
+                    <span class="free" v-if="video_list_item.vip===0">免费</span>
+                    <span class="vip" v-if="video_list_item.vip===10">VIP</span>
+                  </div>
+                  <div class="bottom_right_corner">{{video_list_item.score}}</div>
+                </div>
+                <span class="name">
+                  {{video_list_item.name}}
+                </span>
+                <span class="summary">
+                  {{video_list_item.summary}}
+                </span>
+              </div>
+            </div>
+            <!-- <div class="video_list_container" 
+              v-if="video_content_item.change_block_list"
+            >
+              <div class="video_list" 
+                v-for="(video_list_item, video_list_index) of video_content_item.list.slice().reverse()"
+                :key="video_list_index" 
+                @click="route_to('video_details')"
+              >
+                <div class="img_container">
+                  <img lazyload :src="video_list_item.img" alt="">
+                  <div class="top_left_corner">
+                    <div class="triangle"></div>
+                    <span class="free" v-if="video_list_item.vip===0">免费</span>
+                    <span class="vip" v-if="video_list_item.vip===10">VIP</span>
+                  </div>
+                  <div class="bottom_right_corner">{{video_list_item.score}}</div>
+                </div>
+                <span class="name">
+                  {{video_list_item.name}}
+                </span>
+                <span class="summary">
+                  {{video_list_item.summary}}
+                </span>
+              </div>
+            </div> -->
           </div>
 
           <div class="change">
@@ -463,6 +599,93 @@ export default {
       // border: 1px solid #000;
       padding: 9px 11px;
       .video_list {
+        // border: 1px solid #000;
+        .img_container {
+          // width: 100%;
+          // height: 195px;
+          display: flex;
+          justify-content: center;
+          // border: 1px solid #000;
+          padding: 0 4px;
+          position: relative;
+          img {
+            object-fit: contain;
+            width: 100%;
+            height: 100%;
+          }
+          .top_left_corner {
+            position: absolute;
+            // border: 1px solid #f09;
+            /* width: 164px; */
+            height: 15px;
+            top: 0;
+            left: 0;
+            margin: 1px 0 0 4px;
+            font-size: 10px;
+            font-weight: 400;
+            color: #fff;
+            padding: 0 0 0 8px;
+            // background-color: rgba(162,162,182,.5);
+            .triangle {
+              // border: 6px solid;
+              // border-color: transparent transparent transparent #FF6600;
+              border-left: 7px solid #FF6600;
+              border-right: 7px solid transparent;
+              border-top: 5px solid transparent;
+              border-bottom: 5px solid transparent;
+              width: 0;
+              height: 0;
+              position: absolute;
+              display: inline-block;
+              top: 3px;
+              left: 0;
+            }
+            span {
+              font-size: 10px;
+              font-weight: 400;
+              color: #fff;
+            }
+          }
+          .bottom_right_corner {
+            position: absolute;
+            height: 17px;
+            bottom: 0;
+            right: 0;
+            margin: 0 10px 4px 0;
+            font-size: 10px;
+            font-weight: 400;
+            color: #fff;
+            background-color: rgba(162, 162, 182, 0.5);
+          }
+        }
+        .name {
+          display: block;
+          padding: 0 0 0 4px;
+          width: calc(100% - 0px);
+          line-height: 33px;
+          color: #000028;
+          font-size: 14px;
+          font-weight: 400;
+          position: relative;
+        }
+        .summary {
+          display: block;
+          padding: 0 0 11px 4px;
+          width: calc(100% - 0px);
+          line-height: 14px;
+          color: #a2a2b6;
+          font-size: 13px;
+          font-weight: 400;
+          position: relative;
+        }
+      }
+    }
+    .video_list_top_container {
+      display: grid;
+      grid-template-columns: 1fr;
+      // border: 1px solid #000;
+      padding: 9px 11px;
+      .video_list_top {
         // border: 1px solid #000;
         .img_container {
           // width: 100%;
