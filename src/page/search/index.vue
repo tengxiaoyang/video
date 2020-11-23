e53a73ae8777e90403c760f006ea07ba61c54dc0
txy <656877449@qq.com>
2020-11-23 11:35:06 +0800

    新增或修改

--- src/page/search/index.vue
+++ src/page/search/index.vue
@@ -1,7 +1,20 @@
 <template>
-  	<div>
-      
-  	</div>
+  <div class="main">
+    <div class="header">
+      <div class="search_logo"></div>
+      <div class="search_box">
+        <div class="search_box_inner">
+          <input class="search_text" type="search" :placeholder="search_text">
+          <div class="clear_button"></div>
+        </div>
+      </div>
+      <div class="search_cancel">返回</div>
+    </div>
+    <div class="search_history"></div>
+    <div class="tab"></div>
+    <div class="list"></div>
+    
+  </div>
 </template>
 
 <script>
@@ -9,7 +22,7 @@ export default {
   name: 'search',
   data() {
     return {
-      
+      search_text: "使徒行者3"
     }
   },
   created(){
@@ -20,6 +33,35 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    
+.main {
+  .header {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    position: fixed;
+    width: 100%;
+    .search_logo {
+      background: url(https://vm.gtimg.cn/tencentvideo/vstyle/mobile/v2/style/img/common/search_logo.png);
+      background-size: cover;
+      width: 75px;
+      height: 29px;
+    }
+    .search_box {
+      flex: 1;
+      height: 29px;
+      padding: 0 10px;
+      margin: 0 10px;
+      border-radius: 33px;
+      // background: #fff;
+      background-image: linear-gradient(90deg,#5dcf0c 0,#2dbe8f 30%,#03b0ff);
+      .search_box_inner {
+
+      }
+    }
+    .search_cancel {
+      width: 62px;
+    }
+  }
+}
 
 </style>
